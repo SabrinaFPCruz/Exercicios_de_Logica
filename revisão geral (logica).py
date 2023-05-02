773d86c594473639e77c3f9eebf7b2dbb1e1995a
Sabrina Cruz <128256976+sabrinafpcruz@users.noreply.github.com>
2023-05-02 11:28:46 -0300

    Update revisão geral (logica).py

--- revisão geral (logica).py
+++ revisão geral (logica).py
@@ -115,9 +115,9 @@ num2=int(input("Digite um número: "))
 num3=int(input("Digite um número: "))
 if num1>num2 and num1>num3:
     print(num1)
-if num2>num3 and num2>num1:
+elif num2>num3:
     print(num2)
-if num3>num1 and num3>num2:
+else:
     print(num3)
 
 
