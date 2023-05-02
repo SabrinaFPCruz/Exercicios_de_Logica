74e3e878028ae836ec86720e0eb5a1995d072eb0
Sabrina Cruz <128256976+sabrinafpcruz@users.noreply.github.com>
2023-05-01 22:07:15 -0300

    Update login(vetor).py

--- login(vetor).py
+++ login(vetor).py
@@ -2,12 +2,12 @@ nomes=[]
 senhas=[]
 cont=0
 
-for a in range(3):
+for a in range(5):
     nomes.append(input("Digite seu nome: "))
     senhas.append(input("Digite a senha: "))
 loginNomes=input("Digite seu login: ")
 loginSenha=input("Digite sua senha: ")
-for b in range(3):
+for b in range(5):
     if loginNomes==nomes[b] and loginSenha==senhas[b]:
         print("Login efetuado")
         break
