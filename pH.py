ae56d835e4f7aecb43273a0ced59fe5b0bfb509e
Sabrina Cruz <128256976+sabrinafpcruz@users.noreply.github.com>
2023-05-03 20:15:40 -0300

    Add files via upload

---
+++ pH.py
@@ -0,0 +1,7 @@
+num=int(input("DIgite o indice de acidez de uma soluçao: "))
+if num >= 7:
+    prin("A soluçao e acida")
+elif num <=7:
+    print("A soluçao e base")
+else:
+    print("A soluçao e neutra")
